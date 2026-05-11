8356507ce02dd2d0e5d6ce1dfc47c39dd0008e19
Caua-jove <cauajove2@gmail.com>
2026-05-11 11:28:15 -0300

    Create 67.cs

---
+++ volume e circunferencia/labubu67/67.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+
+            Console.Write("Entre com o valor do Raio: ");
+
+            double raio = double.Parse(Console.ReadLine());
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
